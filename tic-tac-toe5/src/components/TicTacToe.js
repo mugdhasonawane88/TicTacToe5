30e31dd018795ee0cee95213daae12b8e41ce4f1
mugdha <mugdhasonawane88@gmail.com>
2022-07-08 09:42:49 +0530

    feat: display board with 3x3 squares

--- tic-tac-toe5/src/components/TicTacToe.js
+++ tic-tac-toe5/src/components/TicTacToe.js
@@ -2,11 +2,27 @@ import './TicTacToe.css';
 import { Constants } from '../constants/Constants';
 
 function TicTacToe() {
+
+  const renderBoard = () => {
+    return (
+      <div className='board'>
+        {
+          [...Array(Constants.TOTAL_SQUARES)].map((square, index) => {
+            return (
+              <span key={index} role='square' className='cell' />
+            )
+          }
+          )
+        }
+      </div>)
+  }
+
   return (
     <div className='App'>
       <header data-testid='header' className='App-header'>
         {Constants.HEADER}
       </header>
+      {renderBoard()}
     </div>
   );
 }
--- tic-tac-toe5/src/components/TicTacToe.test.js
+++ tic-tac-toe5/src/components/TicTacToe.test.js
@@ -1,13 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import TicTacToe from './TicTacToe';
-import { Constants} from '../constants/TestConstants';
+import { Constants } from '../constants/TestConstants';
 
 describe('TicTacToe component', () => {
+
   test('should have title', () => {
     render(<TicTacToe />);
 
     const headerElement = screen.getByTestId('header');
+
     expect(headerElement).toBeInTheDocument();
     expect(headerElement.textContent).toBe(Constants.HEADER);
   });
+
+  test('Should create empty nine squares in the board when game starts', () => {
+    render(<TicTacToe />);
+
+    const squares = screen.queryAllByRole('square');
+
+    expect(squares).toHaveLength(Constants.TOTAL_SQUARES);
+    squares.forEach((square) => {
+      expect(square.textContent).toBe('');
+    })
+  });
+
 });
